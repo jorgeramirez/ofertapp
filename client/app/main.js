c323695ac394422a3ca20c2c4237af72385cff27
Santiago Valdez <santiago@greek-hp-nb.(none)>
2012-12-16 12:12:45 -0300

    cambio api caralibro

--- client/app/main.js
+++ client/app/main.js
@@ -22,7 +22,7 @@ require(
   function( require, _, Backbone ) {
     // framework has been loaded
     
-    window.SERVER_URL = '/ofertapp/server/index.php';
+    window.SERVER_URL = '/server/index.php';
 
     require( [ 'require',  'app'],
       function() {
