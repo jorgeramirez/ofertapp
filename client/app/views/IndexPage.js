1beef0eb858f605fe6bb2d969b0664c3126ccf80
Jorge Ramírez <jorgeramirez1990@gmail.com>
2012-12-15 23:50:06 -0300

    search flow ui mockup

---
+++ client/app/views/IndexPage.js
@@ -0,0 +1,22 @@
+define(
+  [ 'backbone' ],
+  function( Backbone ) {
+    var IndexPage = Backbone.View.extend( {
+      el: '#index',
+
+      events: {
+        'click a[data-role=button]': 'loginHandler' 
+      },
+
+      initialize: function() {
+        ofertapp.auth.facebook.init();
+      },
+
+      loginHandler: function() {
+        console.log( 'hola' );
+      }
+    } );
+    
+    return IndexPage;
+  }
+);
